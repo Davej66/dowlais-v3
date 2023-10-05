202f7893914c0d72d2bc655286c6b612b1d753d6
Davej66 <daijacobs@gmail.com>
2023-10-05 17:47:10 +0000

    Updating aws settimg

--- dowlais_pharmacy/settings.py
+++ dowlais_pharmacy/settings.py
@@ -160,13 +160,7 @@ LOGIN_URL = '/registration/login/'
 
 
 
-if 'USE_AWS' in os.environ:
-    # Cache control
-    AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=94608000',
-    }
-    
+ 
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'ph-georgetown'
     AWS_S3_REGION_NAME = 'eu-west-2'
@@ -174,13 +168,4 @@ if 'USE_AWS' in os.environ:
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-    # Static and media files
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-    STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-    MEDIAFILES_LOCATION = 'media'
-
-    # Override static and media URLs in production
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-
+   
