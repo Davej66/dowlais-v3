9953c1e9ef601667fe2d44f90344f393d9e22047
Davej66 <daijacobs@gmail.com>
2023-10-06 12:52:19 +0000

    Updating aws settimg

--- dowlais_pharmacy/settings.py
+++ dowlais_pharmacy/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['8000-davej66-dowlaisv3-eidqgx261uj.ws-eu105.gitpod.io','dowlais-pharmacy-80d10d3c17cb.herokuapp.com']
 
 
